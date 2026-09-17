a2342ce71ad8593cefaf3245d50283286eb9b733
tim stanford <tstanford@gmail.com>
2026-09-17 22:01:14 +0100

    Add deployment of sample container

--- install_k3s_cluster.sh
+++ install_k3s_cluster.sh
@@ -3,3 +3,8 @@ ansible-playbook -i servers.ini --private-key ~/.ssh/id_ed25519 optimise-servers
 ansible-playbook -i servers.ini --private-key ~/.ssh/id_ed25519 k3s-cluster.yml
 
 KUBECONFIG=./kubeconfig/k3s-master/etc/rancher/k3s/k3s.yaml kubectl get nodes
+KUBECONFIG=./kubeconfig/k3s-master/etc/rancher/k3s/k3s.yaml kubectl apply -f ./example_deployment/sparkysite.yaml 
+
+KUBECONFIG=./kubeconfig/k3s-master/etc/rancher/k3s/k3s.yaml kubectl rollout status deployment/sparky-site -n sparky
+KUBECONFIG=./kubeconfig/k3s-master/etc/rancher/k3s/k3s.yaml kubectl get all -n sparky
+xdg-open http://192.168.0.60
