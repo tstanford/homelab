2128c4cf919fea838dc4a0e479a221946c9fdef1
tim stanford <tstanford@gmail.com>
2026-09-18 21:48:42 +0100

    Add helloapi deployment

--- install_k3s_cluster.sh
+++ install_k3s_cluster.sh
@@ -2,9 +2,16 @@
 ansible-playbook -i servers.ini --private-key ~/.ssh/id_ed25519 optimise-servers.yml
 ansible-playbook -i servers.ini --private-key ~/.ssh/id_ed25519 k3s-cluster.yml
 
-KUBECONFIG=./kubeconfig/k3s-master/etc/rancher/k3s/k3s.yaml kubectl get nodes
-KUBECONFIG=./kubeconfig/k3s-master/etc/rancher/k3s/k3s.yaml kubectl apply -f ./example_deployment/sparkysite.yaml 
+export KUBECONFIG=./kubeconfig/k3s-master/etc/rancher/k3s/k3s.yaml
 
-KUBECONFIG=./kubeconfig/k3s-master/etc/rancher/k3s/k3s.yaml kubectl rollout status deployment/sparky-site -n sparky
-KUBECONFIG=./kubeconfig/k3s-master/etc/rancher/k3s/k3s.yaml kubectl get all -n sparky
+kubectl get nodes
+
+# kubectl apply -f ./example_deployment/helloapi.yaml 
+# kubectl rollout status deployment/helloapi -n hello
+# kubectl get all -n hello
+# xdg-open http://192.168.0.59/hello
+
+kubectl apply -f ./example_deployment/sparkysite.yaml 
+kubectl rollout status deployment/sparky-site -n sparky
+kubectl get all -n sparky
 xdg-open http://192.168.0.60
